ab63220d508c6f4e6a48f782603be188fcdd1bac
Arif Esat Yilmaz <arif_esat@outlook.com>
2023-12-31 11:53:50 +0300

    added aniamtions to landing page fixed navbar override

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// Changes the color of the about page navbar when the user scrolls down the page
-
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar-about-page');
-    if (window.scrollY > 10 ) { // change 0 to the number of pixels at which you want the color to change
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-
-// Changes the color of the home page navbar when the user scrolls down the page
-
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
     if (window.scrollY > 20 ) { // change 0 to the number of pixels at which you want the color to change
