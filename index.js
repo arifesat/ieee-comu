6d401a4f3e32b114785a75b6f3d39ea113714595
Arif Esat Yilmaz <arif_esat@outlook.com>
2023-12-31 12:04:29 +0300

    edited navbar size

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar-brand');
+    if (window.scrollY > 20 ) { // change 0 to the number of pixels at which you want the color to change
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
